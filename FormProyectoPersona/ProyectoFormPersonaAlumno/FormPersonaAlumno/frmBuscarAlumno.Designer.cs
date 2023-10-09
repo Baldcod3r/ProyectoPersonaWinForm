82c7f94db205fa308c6d7e1290096ac9bfc3f921
Agustin Barrale <agustinbarrale@gmail.com>
2023-10-09 19:47:21 -0300

    commit form buscar

---
+++ FormProyectoPersona/ProyectoFormPersonaAlumno/FormPersonaAlumno/frmBuscarAlumno.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace FormPersonaAlumno
+{
+    partial class frmBuscarAlumno
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lstBoxAlumno = new System.Windows.Forms.ListBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAgregar);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Location = new System.Drawing.Point(91, 17);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(747, 149);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Buscar Alumno";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(219)))), ((int)(((byte)(218)))));
+            this.btnAgregar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAgregar.FlatAppearance.BorderSize = 0;
+            this.btnAgregar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAgregar.Location = new System.Drawing.Point(606, 50);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(5);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(133, 37);
+            this.btnAgregar.TabIndex = 11;
+            this.btnAgregar.Text = "Buscar";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 60);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Nombre del Alumno:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(156, 57);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(5);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(420, 22);
+            this.txtNombre.TabIndex = 6;
+            // 
+            // lstBoxAlumno
+            // 
+            this.lstBoxAlumno.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(219)))), ((int)(((byte)(218)))));
+            this.lstBoxAlumno.FormattingEnabled = true;
+            this.lstBoxAlumno.ItemHeight = 16;
+            this.lstBoxAlumno.Location = new System.Drawing.Point(91, 174);
+            this.lstBoxAlumno.Margin = new System.Windows.Forms.Padding(5);
+            this.lstBoxAlumno.Name = "lstBoxAlumno";
+            this.lstBoxAlumno.Size = new System.Drawing.Size(745, 388);
+            this.lstBoxAlumno.TabIndex = 13;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnSalir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSalir.FlatAppearance.BorderSize = 0;
+            this.btnSalir.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkGreen;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalir.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnSalir.Location = new System.Drawing.Point(915, 264);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(5);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(133, 37);
+            this.btnSalir.TabIndex = 16;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // frmBuscarAlumno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(91)))), ((int)(((byte)(131)))));
+            this.ClientSize = new System.Drawing.Size(1422, 672);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.lstBoxAlumno);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmBuscarAlumno";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmBuscarAlumno";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.ListBox lstBoxAlumno;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
---
+++ FormProyectoPersona/ProyectoFormPersonaAlumno/FormPersonaAlumno/frmBuscarAlumno.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormPersonaAlumno
+{
+    public partial class frmBuscarAlumno : Form
+    {
+        public frmBuscarAlumno()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            string alumno = txtNombre.Text;
+            string path = "C:\\Users\\Facundo\\OneDrive\\Escritorio\\registroAlumnos\\Alumno.txt";
+
+            StreamReader archivoALeer = File.OpenText(path);
+            string renglon;
+            bool encuentra = false;
+            bool finEncuentra = false;
+            List<string> listAlumno = new List<string>();
+            do
+            {
+                renglon = archivoALeer.ReadLine();
+                if (alumno == renglon)
+                {
+                    encuentra = true;
+                }
+                if (encuentra && renglon == "*")
+                {
+                    listAlumno.Add(renglon);
+                    finEncuentra = true;
+                    encuentra = false;
+                }
+                else
+                {
+                    finEncuentra = false;
+                }
+                if (encuentra)
+                {
+                    listAlumno.Add(renglon);
+                }
+
+            } while (renglon != null);
+            archivoALeer.Close();
+            foreach(string a in listAlumno)
+            {
+                lstBoxAlumno.Items.Add(a);
+            }
+
+
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
